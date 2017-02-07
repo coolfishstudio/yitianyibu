6b24d8840aacfd2a419e7f92db5230cbc20fab5a
coolfish <461836324@qq.com>
2017-02-07 20:26:03 +0800

    增加后台管理结构路由跳转

--- src/router.js
+++ src/router.js
@@ -16,10 +16,14 @@ router.get('/admin/login', (req, res) => {
     log('router').info('访问后台登录页')
     res.renderAdminPage('login', { title: '一天一步网', message: '模版测试' })
 })
-
-router.get('/admin/', (req, res) => {
-    log('router').info('访问后台首页')
-    res.renderAdminPage('index', { title: '一天一步网', message: '模版测试' })
-})
+/* 后台管理 */
+router.get('/admin/', (req, res) => { res.renderAdminPage('index') })// 基础平台
+router.get('/admin/app', (req, res) => { res.renderAdminPage('app') })// 应用管理
+router.get('/admin/content', (req, res) => { res.renderAdminPage('content') })// 内容管理
+router.get('/admin/message', (req, res) => { res.renderAdminPage('message') })// 留言管理
+router.get('/admin/category', (req, res) => { res.renderAdminPage('category') })// 类别管理
+router.get('/admin/tag', (req, res) => { res.renderAdminPage('tag') })// 标签管理
+router.get('/admin/stat', (req, res) => { res.renderAdminPage('stat') })// 统计分析
+router.get('/admin/system', (req, res) => { res.renderAdminPage('system') })// 系统管理
 
 export default router
