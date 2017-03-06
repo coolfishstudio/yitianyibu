282b0dfaad3c3fe03d217e12423e5ca192f2d152
coolfish <461836324@qq.com>
2017-03-06 10:27:37 +0800

    打包

--- dist/middleware/error.js
+++ dist/middleware/error.js
@@ -11,12 +11,25 @@ var _log2 = _interopRequireDefault(_log);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = function () {
-    return [function (req, res, next) {
+    return [function (req, res) {
         // 404
         var err = new Error('Not Found');
         err.status = 404;
         (0, _log2.default)('error-404').error(err);
-        next(err);
+        res.status(err.status || 400).format({
+            html: function html() {
+                res.renderPage('error', {
+                    errcode: 404,
+                    message: 'Not Found'
+                });
+            },
+            json: function json() {
+                res.json({
+                    errcode: err.status || 404,
+                    message: 'Not Found'
+                });
+            }
+        });
     }, function (err, req, res, next) {
         if (!err) return next();
         (0, _log2.default)('error-500').error(err);
@@ -24,12 +37,13 @@ exports.default = function () {
         res.status(err.status || 500).format({
             html: function html() {
                 res.renderPage('error', {
+                    errcode: 500,
                     message: message
                 });
             },
             json: function json() {
                 res.json({
-                    code: err.status || 500,
+                    errcode: err.status || 500,
                     message: message
                 });
             }
