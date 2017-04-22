b5effd8a8c946b0d87a2eb64ed9d098646dc5c63
coolfish <461836324@qq.com>
2017-04-22 21:30:14 +0800

    统计规划

--- src/lib/stat/controller.js
+++ src/lib/stat/controller.js
@@ -5,6 +5,13 @@ const viewAdminPage = (req, res) => {
     res.renderAdminPage('stat')
 }
 
+// 获取某人今天第几次访问
+// 获取某人一共访问几次
+// 获取今天的pv
+// 获取今天的uv
+// 获取所有的pv
+// 获取所有的uv
+
 export default {
     viewAdminPage
 }
