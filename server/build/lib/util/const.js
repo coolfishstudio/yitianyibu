f37301f1e9080349c64cf689de1e22344b216828
yves <461836324@qq.com>
2017-12-28 23:19:57 +0800

    v2

--- server/build/lib/util/const.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var APP_LIMIT_DEFAULT = exports.APP_LIMIT_DEFAULT = 100;
-var MESSAGE_LIMIT_DEFAULT = exports.MESSAGE_LIMIT_DEFAULT = 100;
-//# sourceMappingURL=const.js.map
